2890f761255dc4305891de33434e93b54e1861e6
HtooHunn <htetmyetnaing433@gmail.com>
2020-06-25 20:06:18 +0630

    First commit

---
+++ Tuple.py
@@ -0,0 +1,34 @@
+Tuple - ()
+
+
+
+t = 12345, 54321, 'hello'
+t[0]
+
+t
+
+# Tuple may br nested:
+u = t, (1,2,3,4,5)
+u
+
+#Tuples are immutable:
+t[0] = 88888
+
+
+#but they can contain mutable objects:
+v = ([1,2,3], [3,2,1])
+v
+
+fruits = (["apple", "banana", "cheery", "organe", "kiwi", "melon", "mango"])
+print(fruits[2:5])
+
+#change Tuples Value
+
+x = ("apple", "banana", "cheery", "organe")
+y = lsit(x)
+y[1] = "mango"
+x = tuple(y)
+x
+
+>>>Set
+
